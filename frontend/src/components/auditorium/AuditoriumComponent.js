0ead7e67d15b314a60cc70de581189ef4fce62b7
noenemy <noenemy@gmail.com>
2022-01-24 12:46:04 +0900

    Add AuditoriumList mockup page

--- frontend/src/components/auditorium/AuditoriumComponent.js
+++ frontend/src/components/auditorium/AuditoriumComponent.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import AuditoriumList from './AuditoriumList';
 
 class AuditoriumComponent extends Component {
     render() {
         return (
             <div>
-                Auditorium
+                <h2>Auditorium</h2>
+                Amazon IVS를 이용해서 라이브스트리밍을 구현한 대강당 기능입니다.
+                <p></p>
+                <AuditoriumList />
             </div>
         );
     }
---
+++ frontend/src/components/auditorium/AuditoriumList.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import { Card, Button } from 'react-bootstrap';
+
+class AuditoriumList extends Component {
+    constructor(props, context) {
+        super(props, context);
+    }
+
+    createAuditorium() {
+        alert('create');
+    }
+
+    enterAuditorium(auditoriumId) {
+        window.location.href = `/auditorium/${auditoriumId}`;
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <Button variant="primary" onClick={this.createAuditorium}>+ Create an Auditorium</Button>
+                            <p></p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <h4>Which auditorium would you like to enter?</h4>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="holder.js/100px180" />
+                                <Card.Body>
+                                    <Card.Title>대강의실 #1</Card.Title>
+                                    <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk of
+                                    the card's content.
+                                    </Card.Text>
+                                    <Button variant="primary" onClick={() => this.enterAuditorium(1)}>Go somewhere</Button>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        <div class="col">
+                        <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="holder.js/100px180" />
+                                <Card.Body>
+                                    <Card.Title>대강의실 #2</Card.Title>
+                                    <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk of
+                                    the card's content.
+                                    </Card.Text>
+                                    <Button variant="primary" onClick={() => this.enterAuditorium(2)}>Go somewhere</Button>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                        <div class="col">
+                        <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src="holder.js/100px180" />
+                                <Card.Body>
+                                    <Card.Title>대강의실 #3</Card.Title>
+                                    <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk of
+                                    the card's content.
+                                    </Card.Text>
+                                    <Button variant="primary" onClick={() => this.enterAuditorium(3)}>Go somewhere</Button>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default AuditoriumList;
