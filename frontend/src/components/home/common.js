9a018a20c2b09271fa614e823443a8f746df1abb
Jihye Kwon <kwojihye@amazon.com>
2022-02-10 23:59:52 +0900

    공통함수 초더하기 추가

--- frontend/src/components/home/common.js
+++ frontend/src/components/home/common.js
@@ -1,3 +1,8 @@
 export const randomInt = (low, high) => {
 	return Math.floor(Math.random() * (high - low) + low);
 }
+
+export const getTtlSeconds = (ss) => {
+	const current = parseInt(new Date().getTime()/1000);
+	return (current + ss);
+}
