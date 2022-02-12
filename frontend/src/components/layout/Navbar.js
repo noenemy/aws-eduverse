2531363e46a977b4f408e1fdcbe20691d1990040
Jihye Kwon <kwojihye@amazon.com>
2022-02-12 18:26:38 +0900

    Navbar refactor

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -11,10 +11,6 @@ const Navbar  = (props) => {
 
     const user = useRecoilValue(userState);
 
-    useEffect(()=> {
-        console.log("@ Navbar.user >", user)
-    }, [user])
-
     const menu = useMemo(()=> ([
         { menuName: 'Home', to:'/' },
         { menuName: 'Auditorium', to:'/auditorium' },
@@ -43,24 +39,6 @@ const Navbar  = (props) => {
                                 <li key={index} className="nav-item">
                                     <Link className="nav-link" to={item.to}>{item.menuName}</Link>
                                 </li>)}
-                            {/* <li className="nav-item">
-                                <a className="nav-link" href="/home">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/auditorium">Auditorium</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/classroom">Classroom</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/vrlearning">VR Learning</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/lounge">Lounge</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/about">About</a>
-                            </li> */}
                             <li className="nav-item dropdown mx-5">
                                 <a className="nav-link dropdown-toggle" href="#" id="dropdown09" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span className="flag-icon flag-icon-kr"> </span> 한국어</a>
                                 <div className="dropdown-menu" aria-labelledby="dropdown09">
@@ -74,7 +52,7 @@ const Navbar  = (props) => {
                                 <a className="nav-link" href="#">{ user && user.nickname ? <i className="far fa-user">{" "+user.nickname}</i> : ''}</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">{ user && user.id ? 'Exit' : 'Please log in'}</a>
+                                <a className="nav-link" href="#">{ user && user.id ? 'Exit' : ''}</a>
                             </li>
                         </ul>
                     </div>
