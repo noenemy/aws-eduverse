c508c343270f2a5384f33ace6db59d50d8b9d1cf
noenemy <noenemy@gmail.com>
2022-02-19 00:52:29 +0900

    Implement to get VR learning data by using graphQL query

--- frontend/src/components/vrlearning/VRLearning.js
+++ frontend/src/components/vrlearning/VRLearning.js
@@ -2,32 +2,68 @@ import React, { Component, Fragment } from 'react';
 import { Button } from "react-bootstrap";
 import { ToastContainer, toast } from 'react-toastify';
 import LoadScreen from '../../assets/images/load_screen.png'
+import { API, graphqlOperation } from 'aws-amplify';
+import { listCourses, listLectures } from '../../graphql/queries';
+import ReactLoading from 'react-loading';
+//import axios from "axios";
 
 class VRLearningComponent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            courses: [{
-                id: "1",
-                language: "ko-KR",
-                icon: "asia",
-                course_title: "한국어"
-            },
-            {
-                id: "2",
-                language: "es-ES",
-                icon: "europe",
-                course_title: "Spanish"
-            }],
-            lectures: [{
-                id: "1",
-                lecture_title: "1강. 인사말을 배워봅시다."
-            }, 
-            {
-                id: "2",
-                lecture_title: "2강. 드라마로 배우는 한국어 표현"
-            }]
-        };
+    state = {
+        selectedCourseId: "ef717ea27fc4426b99e484343a493e5a", // Korean 101
+        courses: null,
+        language: 'ko-KR',
+        lectures: null,
+        loading: false
+    };
+
+    componentDidMount() {
+        this.getCourses();
+        this.getLectures(this.state.selectedCourseId);
+    }
+
+    selectCourse = (courseId, language) => {
+        this.setState({ selectedCourseId: courseId, language: language }, () => {
+            this.getLectures(courseId);
+        });
+    }
+
+    async getCourses() {
+        this.setState({ loading: true });
+        const res = await API.graphql(graphqlOperation(listCourses));
+        //console.log(res);
+        this.setState({ loading: false });
+
+        if (res != null && res.data.listCourses != null) {
+            this.setState({ courses: res.data.listCourses.items });
+        }
+        else {
+            toast.error("something wrong! try again.");
+        }
+    }
+
+    async getLectures(courseId="ef717ea27fc4426b99e484343a493e5a") {
+        this.setState({ loading: true });
+        console.log('@search > ' + courseId); 
+        const res = await API.graphql(graphqlOperation(listLectures, {
+            filter: {
+                course_ref: {
+                    eq: courseId
+                }
+            }
+        }));
+        console.log(res);     
+        this.setState({ loading: false });
+
+        if (res != null && res.data.listLectures != null) {
+            this.setState({ lectures: res.data.listLectures.items });
+        }
+        else {
+            toast.error("something wrong! try again.");
+        }
+    }
+
+    enterClassroom = (lectureId, language) => {
+        window.location.href = `/classroom?courseId=${this.state.selectedCourseId}&lectureId=${lectureId}&language=${language}`;
     }
 
     render() {
@@ -37,14 +73,10 @@ class VRLearningComponent extends Component {
                 Amazon AI/ML 서비스 및 Sumerian을 이용해서 구현한 셀프스터디 서비스입니다.
                 <div className="container">
                     <div className="row">
-                        <br /><br />
-                    </div>
-                    <div className="row">
                         <div className="col-6">
                             <img className="img-fluid" src={LoadScreen} alt="loadscreen" width="500" height="400"></img>
                         </div>
                         <div className="col-6">
-                            <br /><br />
                             <h2>
                                 <i className="fas fa-quote-left" />&nbsp;
                                 Hi, we are your language tutors.
@@ -57,7 +89,7 @@ class VRLearningComponent extends Component {
                             {this.state.courses && this.state.courses.map(( course, index) => {
                                     return (
                                         <Fragment>
-                                        <Button className="ml-2" key={course.id} onClick={() => this.selectCourse(course.id, course.language)}><i className={ "fas fa-globe-" + course.icon } />&nbsp;{ course.course_title }</Button>
+                                        <Button className="ml-2" key={course.id} onClick={() => this.selectCourse(course.id, course.language)}><i className={ "fas fa-globe-" + course.icon } />&nbsp;{ course.title }</Button>
                                         &nbsp;&nbsp;</Fragment>
                                         );
                             })}
@@ -71,7 +103,7 @@ class VRLearningComponent extends Component {
                                 {this.state.lectures && this.state.lectures.map(( lecture, index) => {
                                     return (
                                         <button type="button" className="list-group-item list-group-item-action" key={lecture.id} onClick={() => this.enterClassroom(lecture.id, this.state.language)}>
-                                            <i className="fas fa-book" />&nbsp;{ lecture.lecture_title }
+                                            <i className="fas fa-book" />&nbsp;{ lecture.title }
                                         </button>
                                         );
                                 })}
