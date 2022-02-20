a5cf1f671f51ed46407effcced24127949ecda5a
noenemy <noenemy@gmail.com>
2022-02-20 13:29:19 +0900

    Refactor vrlearning.js to use hook

--- frontend/src/components/vrlearning/VRLearning.js
+++ frontend/src/components/vrlearning/VRLearning.js
@@ -1,136 +1,131 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import { Button } from "react-bootstrap";
 import { ToastContainer, toast } from 'react-toastify';
 import LoadScreen from '../../assets/images/load_screen.png'
 import { API, graphqlOperation } from 'aws-amplify';
 import { searchCourses, searchLectures } from '../../graphql/queries';
+import { useNavigate } from 'react-router-dom';
 import ReactLoading from 'react-loading';
-//import axios from "axios";
 
-class VRLearningComponent extends Component {
-    state = {
-        selectedCourseId: "ef717ea27fc4426b99e484343a493e5a", // Korean 101
-        courses: null,
-        language: 'ko-KR',
-        lectures: null,
-        loading: false
-    };
+const VRLearning = (props) => {
 
-    componentDidMount() {
-        this.getCourses();
-        this.getLectures(this.state.selectedCourseId);
+    const [ selectedCourseId, setSelectedCourseId ] = useState("ef717ea27fc4426b99e484343a493e5a"); // Korean 101
+    const [ courses, setCourses ] = useState();
+    const [ language, setLanguage ] = useState('ko-KR');
+    const [ lectures, setLectures ] = useState();
+    const [ courseLoading, setCourseLoading ] = useState(false);
+    const [ lectureLoading, setLectureLoading ] = useState(false);
+    
+    const navigate = useNavigate();
+
+    useEffect(async () => {
+        getCourses();
+        getLectures(selectedCourseId);
+    }, []);
+
+    const selectCourse = (courseId, language) => {
+       setSelectedCourseId(courseId);
+       setLanguage(language);
+       getLectures(courseId);
     }
 
-    selectCourse = (courseId, language) => {
-        this.setState({ selectedCourseId: courseId, language: language }, () => {
-            this.getLectures(courseId);
-        });
-    }
+    const getCourses = async () => {
+        setCourses(null);
+        setCourseLoading(true);
 
-    async getCourses() {
-        this.setState({ loading: true });
         const res = await API.graphql(graphqlOperation(searchCourses, {
-            sort: {
-                direction: 'asc',
-                field: 'order'
-            }
+            sort: { direction: 'asc', field: 'order' }
         }));
-        //console.log(res);
-        this.setState({ loading: false });
+        setCourseLoading(false);
 
         if (res != null && res.data.searchCourses != null) {
-            this.setState({ courses: res.data.searchCourses.items });
+            setCourses(res.data.searchCourses.items);
         }
         else {
             toast.error("something wrong! try again.");
         }
     }
 
-    async getLectures(courseId="ef717ea27fc4426b99e484343a493e5a") {
-
-        this.setState({ loading: true });
-        console.log('@search > ' + courseId); 
+    const getLectures = async (courseId="ef717ea27fc4426b99e484343a493e5a") => {
+        setLectures(null);
+        setLectureLoading(true);
+        
         const res = await API.graphql(graphqlOperation(searchLectures, {
-            filter: {
-                course_ref: {
-                    eq: courseId
-                }
-            },
-            sort: {
-                direction: 'asc',
-                field: 'order'
-            }
-        }));
-        console.log(res);     
-        this.setState({ loading: false });
+            filter: { course_ref: { eq: courseId } },
+            sort: { direction: 'asc', field: 'order' }
+        })); 
+        setLectureLoading(false);
 
         if (res != null && res.data.searchLectures != null) {
-            const lectures = res.data.searchLectures.items;
-            this.setState({ lectures: lectures });
+            setLectures(res.data.searchLectures.items);
         }
         else {
             toast.error("something wrong! try again.");
         }
     }
 
-    enterClassroom = (lectureId, language) => {
-        window.location.href = `/classroom?courseId=${this.state.selectedCourseId}&lectureId=${lectureId}&language=${language}`;
+    const enterClassroom = (lectureId, language) => {
+        navigate(`/vrlearning/classroom?courseId=${selectedCourseId}&lectureId=${lectureId}&language=${language}`);
     }
 
-    render() {
-        return (
-            <div>
-                <h2>VRLearning</h2>
-                Amazon AI/ML 서비스 및 Sumerian을 이용해서 구현한 셀프스터디 서비스입니다.
-                <div className="container">
-                    <div className="row">
-                        <div className="col-6">
-                            <img className="img-fluid" src={LoadScreen} alt="loadscreen" width="500" height="400"></img>
-                        </div>
-                        <div className="col-6">
-                            <h2>
-                                <i className="fas fa-quote-left" />&nbsp;
-                                Hi, we are your language tutors.
-                            </h2>
-                            <h2>Learning a language is fun. </h2>
-                            <h2>We will help you. <i className="fas fa-quote-right" />&nbsp;
-                            </h2>
-                            <br /><br />
-                            <h2>Which language do you want to learn?</h2>
-                            {this.state.courses && this.state.courses.map(( course, index) => {
-                                    return (
-                                        <Fragment>
-                                        <Button className="ml-2" key={course.id} onClick={() => this.selectCourse(course.id, course.language)}><i className={ "fas fa-globe-" + course.icon } />&nbsp;{ course.title }</Button>
-                                        &nbsp;&nbsp;</Fragment>
-                                        );
+    return (
+        <div>
+            <h2>VRLearning</h2>
+            Amazon AI/ML 서비스 및 Sumerian을 이용해서 구현한 셀프스터디 서비스입니다.
+            <div className="container">
+                <div className="row">
+                    <div className="col-6">
+                        <img className="img-fluid" src={LoadScreen} alt="loadscreen" width="500" height="400"></img>
+                    </div>
+                    <div className="col-6">
+                        <h2>
+                            <i className="fas fa-quote-left" />&nbsp;
+                            Hi, we are your language tutors.
+                        </h2>
+                        <h2>Learning a language is fun. </h2>
+                        <h2>We will help you. <i className="fas fa-quote-right" />&nbsp;
+                        </h2>
+                        <br />
+                        <h2>Which language do you want to learn?</h2>
+                        { courseLoading &&
+                            <ReactLoading type="spin" color="#123abc" size="sm" /> 
+                        }
+                        { courses && courses.map(( course, index) => {
+                            return (
+                                <Fragment>
+                                <Button className="ml-2" key={course.id} onClick={() => selectCourse(course.id, course.language)}><i className={ "fas fa-globe-" + course.icon } />&nbsp;{ course.title }</Button>
+                                &nbsp;&nbsp;</Fragment>
+                                );
+                        })}
+                        
+                        <br /><br />
+                        <h2>Which subject do you want to learn today?</h2>
+                        <div className="list-group">
+                            <button type="button" className="list-group-item list-group-item-action list-group-item-secondary" aria-current="true">
+                                Course content
+                            </button>
+                            { lectureLoading &&
+                                <ReactLoading type="spin" color="#123abc" /> 
+                            }
+                            { lectures && lectures.map(( lecture, index) => {
+                                return (
+                                    <button type="button" className="list-group-item list-group-item-action" key={lecture.id} onClick={() => enterClassroom(lecture.id, language)}>
+                                        <i className="fas fa-book" />&nbsp;{ lecture.title }
+                                    </button>
+                                    );
                             })}
-                            
-                            <br /><br />
-                            <h2>Which subject do you want to learn today?</h2>
-                            <div className="list-group">
-                                <button type="button" className="list-group-item list-group-item-action list-group-item-secondary" aria-current="true">
-                                    Course content
-                                </button>
-                                {this.state.lectures && this.state.lectures.map(( lecture, index) => {
-                                    return (
-                                        <button type="button" className="list-group-item list-group-item-action" key={lecture.id} onClick={() => this.enterClassroom(lecture.id, this.state.language)}>
-                                            <i className="fas fa-book" />&nbsp;{ lecture.title }
-                                        </button>
-                                        );
-                                })}
-
-                            </div>
 
                         </div>
+
                     </div>
-                    <div className="row"> 
-    
-                    </div>
-                    <ToastContainer position="bottom-right" autoClose="3000" />
                 </div>
+                <div className="row"> 
+
+                </div>
+                <ToastContainer position="bottom-right" autoClose="3000" />
             </div>
-        );
-    }
+        </div>
+    );
 }
 
-export default VRLearningComponent;
+export default VRLearning;
