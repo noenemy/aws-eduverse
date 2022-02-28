9cbbefa24c50147d601e97679d730cf335f027cb
Jihye Kwon <kwojihye@amazon.com>
2022-02-28 23:45:12 +0900

    lobby scroll top fix

--- frontend/src/components/home/HomeComponent.jsx
+++ frontend/src/components/home/HomeComponent.jsx
@@ -4,6 +4,7 @@ import Chat from '../classroom/Chat';
 import { useRecoilValue } from 'recoil';
 import { userState } from '../../recoil/user/userState';
 import { Box, Grid } from '@awsui/components-react';
+import ScrollToTop from './ScrollToTop';
 
 const HomeComponent = (props) => {
 
@@ -18,12 +19,14 @@ const HomeComponent = (props) => {
 
     if(user && user.id && user.nickname) {
         return (
-            <Grid gridDefinition={[{ colspan: 9 }, { colspan: 3 }]}>
-                <PhaserContainer game={game} setGame={g=>setGame(g)}/>
-                <Box margin={{left:"m", right: "m"}}>
-                    <Chat />
-                </Box>
-            </Grid>
+            <ScrollToTop>
+                <Grid gridDefinition={[{ colspan: 9 }, { colspan: 3 }]}>
+                    <PhaserContainer game={game} setGame={g=>setGame(g)}/>
+                    <Box margin={{left:"m", right: "m"}}>
+                        <Chat />
+                    </Box>
+                </Grid>
+            </ScrollToTop>
         );
     }
 
