97b1cec83a047f497e2ec637d2d3312ea061e032
Jihye Kwon <kwojihye@amazon.com>
2022-03-06 21:22:00 +0900

    gravity 강도 높임

--- frontend/src/components/home/scenes/Splash.js
+++ frontend/src/components/home/scenes/Splash.js
@@ -28,7 +28,7 @@ class Splash extends Phaser.Scene {
         let scale = Math.max(scaleX, scaleY)
         bgImage.setScale(scale).setScrollFactor(0)
 
-        this.topText = this.add.text(bgImage.width / 2 - 33, 300, "AWS 서비스를 이용해서 구현한", { fontSize: 30 });
+        this.topText = this.add.text(bgImage.width / 2 - 33, 300, "AWS 서비스를 이용하여 구현한", { fontSize: 30 });
         this.add.text(bgImage.width / 2 - 20, 350, "인터랙티브 가상 교육 플랫폼", { fontSize: 30 });
 
         this.clickButton = this.add.text(bgImage.width / 2, bgImage.height / 2 + 250, '[ 시작하기 ]', { fill: '#0f0', fontSize: 50 })
@@ -39,7 +39,7 @@ class Splash extends Phaser.Scene {
 
         this.logo = this.physics.add.image(this.cameras.main.width / 2, 150, 'logo_image');
         this.logo.setVelocity(0, 100);
-        this.logo.setGravity(0, 180)
+        this.logo.setGravity(0, 250)
         this.logo.setBounce(1, 1);
         this.logo.setCollideWorldBounds( true );
 
